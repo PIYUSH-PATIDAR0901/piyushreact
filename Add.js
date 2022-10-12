c4c267af9a8fe36d91258b602f89c3a6db74c85b
Piyush patidar <113409772+PIYUSH-PATIDAR0901@users.noreply.github.com>
2022-10-12 11:30:34 +0530

    Add files via upload

---
+++ Add.js
@@ -0,0 +1,11 @@
+export default function Add(){
+
+    let a = 10;
+    let b = 20;
+    let c = a + b;
+    return (
+        <div>
+            <p>Adiition of two number is = {c}</p>
+        </div>
+    )
+}
